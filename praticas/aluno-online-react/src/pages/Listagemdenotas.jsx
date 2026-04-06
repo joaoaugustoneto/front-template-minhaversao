08480f8c3c74d8835d3dc7486e2f5dd8d446353f
Joao Augusto <evangelistanetojoaoaugusto@gmail.com>
2026-04-06 10:25:39 -0300

    add feature/pratica03

---
+++ praticas/aluno-online-react/src/pages/Listagemdenotas.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/Sidebar";
+
+function ListagemDeNotas () {
+    return (
+        <div className="app-container">
+           <Sidebar/>
+           <Topbar/>
+        </div>
+    );
+}
+
+export default ListagemDeNotas;
---
+++ praticas/aluno-online-react/src/pages/Listagemderequerimentos.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/Sidebar";
+
+function ListagemDeRequerimentos () {
+    return (
+        <div className="app-container">
+           <Sidebar/>
+           <Topbar/>
+        </div>
+    );
+}
+
+export default ListagemDeRequerimentos;
