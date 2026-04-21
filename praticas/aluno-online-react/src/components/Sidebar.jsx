0d5d661e4150e121aff7cd1932733ac93e887eff
joaoaugustoeneto <jaofalandoporsi@gmail.com>
2026-04-21 14:02:51 -0300

    feat: conclui pratica05. Fecha #11

--- praticas/aluno-online-react/src/components/Sidebar.jsx
+++ praticas/aluno-online-react/src/components/Sidebar.jsx
@@ -1,15 +1,17 @@
-import './Sidebar.css';
-import logo from '../assets/learn.svg';
 import Menu from './Menu';
+import logo from '../assets/learn.svg'; // Importando a logo do projeto
+import './Sidebar.css';
 
 function Sidebar() {
   return (
     <aside className="sidebar">
-      <figure className="logo">
-        <img src={logo} alt="Imagem de um capelo" />
-        <h1>Aluno Online</h1>
-      </figure>
-      <Menu/>
+      {/* Cabeçalho da Sidebar (Mantendo o estilo antigo) */}
+      <div className="sidebar-header">
+        <img src={logo} alt="Logo Aluno Online" className="sidebar-logo" />
+        <h2>Aluno Online</h2>
+      </div>
+
+      <Menu></Menu>
     </aside>
   );
 }
